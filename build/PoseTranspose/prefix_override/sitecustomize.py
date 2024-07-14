acac10cb4dc287bd89ba63369b55b1025b9fb3a5
kaleo22 <leonard.kaempf@icloud.com>
2024-07-14 21:48:18 +0200

    added git ignore and modified PoseTransposeNode

--- build/PoseTranspose/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/leonard/peak_cam_docker_container/PoseTranspose/install/PoseTranspose'
