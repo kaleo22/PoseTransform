d908ba087ab3ba4cdf8b82377a6da8bcfd114c5f
kaleo22 <leonard.kaempf@icloud.com>
2024-07-17 17:16:09 +0200

    added share directory params/config.yaml to setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' +package_name, ['launch/PoseTransform_launch.py'] )
+        ('share/' +package_name, ['launch/PoseTransform_launch.py'] ),
+        ('share/' +package_name, ['params/config.yaml'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
